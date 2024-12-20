2630b95dde783b5fa36a0bb337d17908c06c8181
abdoforda <abdelrahmaan3@gmail.com>
2024-12-20 15:36:05 +0200

    a

--- resources/views/components/products.blade.php
+++ resources/views/components/products.blade.php
@@ -6,7 +6,7 @@
             @include('components.card_product', ['item' => $item])
         @endforeach
 
-        @if (isset($products->links))
+        @if (isset($products->links()))
 
             {{ $products->links() }}
             
