657037ea6e08442a05ce8414a7cabe523df5476f
abdoforda <abdelrahmaan3@gmail.com>
2025-03-06 14:18:17 +0200

    a

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -13,9 +13,6 @@
     <section class="flat-spacing-9">
         <div class="container">
             <div class="tf-grid-layout gap-0 lg-col-2">
-                <div class="w-100">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d317859.6089702069!2d-0.075949!3d51.508112!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48760349331f38dd%3A0xa8bf49dde1d56467!2sTower%20of%20London!5e0!3m2!1sen!2sus!4v1719221598456!5m2!1sen!2sus" width="100%" height="894" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-                </div>
                 <div class="tf-content-left has-mt">
                     <div class="sticky-top">
                         <h5 class="mb_20">تفضل بزيارة متجرنا</h5>
